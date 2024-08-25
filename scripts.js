3497db08c06dba52b0d1fb570ed5aa1f7312f43f
Jemson <163845844+Rogemson@users.noreply.github.com>
2024-08-25 10:42:52 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,38 @@
 function scrollToAbout() {
     document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.info');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-bounce');
+                observer.unobserve(entry.target); 
+            }
+        });
+    }, {
+        threshold: 0.2 
+    });
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const homeSection = document.querySelector('#home');
+    const elementsToAnimate = homeSection.querySelectorAll('.image-content, .container');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-up');
+                observer.unobserve(entry.target); 
+            }
+        });
+    }, {
+        threshold: 0.1 
+    });
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+});
